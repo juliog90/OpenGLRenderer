08a715170d3db7c643d54f97b9fc7e0d8f2f79f3
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-10-24 20:32:57 -0700

    Primer Commit

---
+++ src/Application.cpp
@@ -0,0 +1,111 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "Shader.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+      if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(480, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    /* Initalize GLEW */
+    if(glewInit() != GLEW_OK) {
+        std::cout << "Error" << std::endl;
+    }
+
+    unsigned int indices[] =
+    {
+        0, 1, 2 ,
+        3, 2, 1
+    };
+
+    float positions[8] {
+        -0.5f, -0.5f, // 0
+        -0.5f,  0.5f, // 1
+         0.5f, -0.5f, // 2
+         0.5f,  0.5f, // 3
+    };
+
+    // Vertex Array
+    VertexArray va;
+
+    // Vertex Buffer
+    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+
+    // Vertex Buffer Layout
+    VertexBufferLayout layout;
+    layout.PushFloat(2);
+    va.AddBuffer(vb, layout);
+
+    // Index Buffer
+    IndexBuffer ib(indices, 6);
+
+    // Shader Setup
+    Shader shader("res/shaders/basic.shader");
+    shader.Bind();
+
+    shader.SetUniforms4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
+    Renderer renderer;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+         renderer.Clear();
+
+         glfwSwapInterval(1);
+         shader.SetUniforms4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+
+
+         renderer.Draw(va, ib, shader);
+
+         if(r > 1.0f)
+         {
+             increment = -0.05f;
+         }
+         else if (r < 0.0f)
+         {
+             increment = 0.05f;
+         }
+
+         r += increment;
+
+        /* Swap front and back buffers */
+         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
